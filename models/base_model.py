11cad75d0b3695dd34a56782d92ea9d1dc7977bf
Sarolus <cactussius.primus@gmail.com>
2021-06-24 16:25:50 +0200

    Fix pep8 & change id, create & update position

--- models/base_model.py
+++ models/base_model.py
@@ -15,33 +15,37 @@ class BaseModel:
         """
         constructor methode
         """
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    new_value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                    setattr(self, key, new_value)
-                elif key != "__class__":
-                    setattr(self, key, value)        
-        
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    new_value = datetime.strptime(
+                        value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, new_value)
+                elif key != "__class__":
+                    setattr(self, key, value)
 
     def __str__(self):
         """
         Return a string repr of BaseModel class
         """
-        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
+        return ("[{}] ({}) {}".format(self.__class__.__name__,
+                                      self.id, self.__dict__))
 
     def save(self):
         """
-        updates the public instance attribute updated_at with the current datetime
+        updates the public instance attribute updated_at
+        with the current datetime.
         """
         self.updated_at = datetime.now()
 
     def to_dict(self):
         """
-        returns a dictionary containing all keys/values of __dict__ of the instance
+        returns a dictionary containing all keys/values of
+        __dict__ of the instance.
         """
         new_dict = {}
         new_dict["__class__"] = self.__class__.__name__
