a2a493b2dce82a03b37796b4c4cbb29e7c09042b
SimonBr017 <simonbrard017@gmail.com>
2021-06-29 15:00:22 +0200

    add test state

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python3
-"""Amenity test class"""
+"""module test classes"""
 
 from models import amenity
 from models.base_model import BaseModel
+from models import state
 import unittest
 Amenity = amenity.Amenity
+State = state.State
 
 
 class TestAmenity(unittest.TestCase):
     """Amenity test class"""
-    def test_name_attr(self):
+    def testName(self):
         """Test Amenity has attribute"""
         amenity = Amenity()
         self.assertTrue(hasattr(amenity, "name"))
         self.assertEqual(amenity.name, "")
+
+
+class TestState(unittest.TestCase):
+    """State test class"""
+    def testName(self):
+        state = State()
+        self.assertTrue(hasattr(state, "name"))
+        self.assertEqual(state.name, "")
